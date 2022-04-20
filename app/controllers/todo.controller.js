63fed9a81df0690c13235e07a5e879c6894def24
kwon-hyemin <kwon9184h@gmail.com>
2022-04-20 12:56:03 +0900

    2022-04-19

--- app/controllers/todo.controller.js
+++ app/controllers/todo.controller.js
@@ -1,3 +1,18 @@
 exports.todo = (req, res) => {
     res.status(200).json(req.body)
 }
+
+// exports.todo = (req, res) => {
+    
+    
+//       res.status(200).json(req.body)
+// }
+
+// exports.todo = (req, res) => {
+//     res.status(200).json(req.body)
+// }
+
+// exports.todo = (req, res) => {
+//     res.status(200).json(req.body)
+// }
+
